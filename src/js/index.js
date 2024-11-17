2c94fed841da466202eff9e678004d232eb92f75
DorianCursos <doriancursos@gmail.com>
2024-11-17 14:22:32 +0100

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-import '../css/styles.css';
+const buttonElement = document.getElementById('button');
+
+const printClick = (event, name) => {
+	console.log(name);
+	console.log(event);
+};
+
+buttonElement.addEventListener('click', event => printClick(event, 'Dorian'));
